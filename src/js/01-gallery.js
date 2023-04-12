6994c3d5a3b5c4e0ae0b30c7f220d6e04ebcfa62
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-04-12 22:48:01 +0300

    HW task1 resolved 12.04.2023

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,15 +3,47 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-{
-  /* <li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+const gallery = document.querySelector(".gallery");
+
+function markupGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+  <a class="gallery__link" href=${original}>
     <img
       class="gallery__image"
-      src="small-image.jpg"
-      data-source="large-image.jpg"
-      alt="Image description"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
     />
   </a>
-</li>; */
+</li>
+`;
+    })
+    .join("");
+}
+gallery.innerHTML = markupGalleryItems(galleryItems);
+
+gallery.addEventListener("click", onModalOpenClickImage);
+
+function onModalOpenClickImage(evt) {
+  evt.preventDefault();
+
+  const image = evt.target;
+  const instance = basicLightbox.create(`
+    <img src=${image.dataset.source} width="800" height="600">
+  `);
+  instance.show();
+
+  window.addEventListener("keydown", onEscModalClose);
+
+  function onEscModalClose(evt) {
+    console.log(evt);
+    if (evt.code !== "Escape") {
+      return;
+    }
+    instance.close();
+    window.removeEventListener("keydown", onEscModalClose);
+  }
 }
