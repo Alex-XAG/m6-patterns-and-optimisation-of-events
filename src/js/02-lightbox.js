f581608c9337e1d7b3b1868853f19b50622f77b5
Alex-XAG <109016896+Alex-XAG@users.noreply.github.com>
2023-04-12 23:55:00 +0300

    m7-Done SimpleLightbox

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+function markupGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+      <a class="gallery__link" href=${original}>
+         <img class="gallery__image" src=${preview} alt=${description} />
+      </a>
+   </li>
+`;
+    })
+    .join("");
+}
+gallery.insertAdjacentHTML("beforeend", markupGalleryItems(galleryItems));
+
+gallery.addEventListener("click", onSimpleLightboxOpen);
+
+function onSimpleLightboxOpen(evt) {
+  evt.preventDefault();
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionSelector: "img",
+    captionType: "attr",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+  lightbox.on("show.lightbox");
+}
